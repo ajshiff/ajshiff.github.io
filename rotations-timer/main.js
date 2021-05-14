c895818ceb989706eae9b17dcfe33bc80dd9ed4a
Aaron Shiffler <ajshiffler@gmail.com>
2021-05-13 22:43:20 -0600

    added rotations timer

---
+++ rotations-timer/main.js
@@ -0,0 +1,58 @@
+(function () {
+    function getAllInputReferences(ids) {
+        let references = {}
+        let kvps = ids.map(id => {
+            return {
+                key: id,
+                value: document.getElementById(id)
+            }
+        })
+        kvps.forEach(kvp => references[kvp.key] = kvp.value)
+        return references
+    }
+
+    function wait(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+
+    async function run(times, soundToPlay, displays) {
+        for (const time of times) {
+            console.log(`starting ${time}ms timer`)
+            displays.minutesRemaining.innerHTML = `${time / 60000}`
+            await wait(time)
+            console.log(`finished waiting ${time}ms`)
+            await soundToPlay()
+        }
+    }
+
+    async function playSound (audioClip, clipDuration, times) {
+        for (let i = 1; i <= times; i++) {
+            console.log("playing..." + i)
+            audioClip.play()
+            await wait (clipDuration)
+        }
+    }
+
+    function initializeEventListeners (inputs, waitTimes, audioElements, displayElements) {
+        inputs.startTimer.addEventListener('click', () => run(waitTimes, async () => await playSound(audioElements.cuckooSound, 2100, 5), displayElements))
+        inputs.playSound.addEventListener('click', () => playSound(audioElements.cuckooSound, 2100, 1))
+    }
+
+    let displayFieldIds = ['minutesRemaining']
+    let inputIds = ['startTimer', 'playSound']
+    let audioIds = ['cuckooSound']
+    let waitTimes = [
+        60000,
+        600000,
+        60000,
+        600000,
+        60000,
+        600000,
+        180000,
+    ]
+
+    let displayElements = getAllInputReferences(displayFieldIds)
+    let audioElements = getAllInputReferences(audioIds)
+    let inputElements = getAllInputReferences(inputIds)
+    initializeEventListeners(inputElements, waitTimes, audioElements, displayElements)
+})()
